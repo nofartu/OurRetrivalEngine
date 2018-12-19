b0328f0a690b1c027463e6b6992a2ec8f18da3e4
nofartu <nofartu@post.bgu.ac.il>
2018-12-19 19:26:37 +0200

    with searcher checkd + bm25

--- src/main/java/sample/Ranker.java
+++ src/main/java/sample/Ranker.java
@@ -3,19 +3,25 @@ package sample;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 import static sample.Indexer.docsCoprus;
 
 public class Ranker {
-    public Ranker() {
+    HashMap<String,Integer> allDocs;
 
+    public Ranker() {
+allDocs=new HashMap<>();
     }
 
     public void rankBM25(HashMap<String, ArrayList<String[]>> docsContainsQuery, HashMap<String, Integer> countWordsQuery, int numOfDocs) {
         double sum = 0;
-        int k = 0, b = 0;
-        double avdl=getAvdl();
+        double k = 1.2, b = 0.75;
+        double avdl = getAvdl();
+        int count = 0;
+        int count2 = 0;
         for (Map.Entry<String, ArrayList<String[]>> entry : docsContainsQuery.entrySet()) {
+
             for (String[] info : entry.getValue()) {
                 String key = info[0]; //word name
                 int cWQ = countWordsQuery.get(key);
@@ -25,8 +31,10 @@ public class Ranker {
                 double tmp = cWQ * (((k + 1) * cWD) / (cWD + k * (1 - b + b * (dLength / avdl)))) * Math.log10((numOfDocs + 1) / df);
                 sum = sum + tmp;
             }
+            System.out.println("doc num: " + entry.getKey() + " and the bm25 is: " + sum);
+            sum = 0;
         }
-        System.out.println(sum);
+
     }
 
     private double getAvdl() {
--- src/main/java/sample/Searcher.java
+++ src/main/java/sample/Searcher.java
@@ -24,8 +24,8 @@ import static sample.ReadFile.mySplit;
 public class Searcher {
     private Parse parse;
     private HashMap<String, ArrayList<String>> wordAndLocations;
-    private HashMap<String,ArrayList<String[]>> docsContainsQuery;
-    private HashMap<String,Integer> countWordsQuery;
+    private HashMap<String, ArrayList<String[]>> docsContainsQuery;
+    private HashMap<String, Integer> countWordsQuery;
     private String postPath;
     private boolean stem;
     private int numOfDocs;
@@ -33,16 +33,16 @@ public class Searcher {
     public Searcher(HashSet<String> stopwords, String stopwordsPath, String postPath, boolean stem, ApiJson apiJson) {
         this.stem = stem;
         HashSet<String> stopWords = stopwords;
-        if (stopwords!=null) {
+        if (stopwords == null) {
             stopWords = createHashStopWords(stopwordsPath);
         }
         parse = new Parse(stopWords, stem, apiJson);
         this.postPath = postPath;
-        this.numOfDocs=docsCoprus.size();
+        this.numOfDocs = docsCoprus.size();
 //        this.numOfDocs=472525;
-        wordAndLocations=new HashMap<>();
-        docsContainsQuery=new HashMap<>();
-        countWordsQuery=new HashMap<>();
+        wordAndLocations = new HashMap<>();
+        docsContainsQuery = new HashMap<>();
+        countWordsQuery = new HashMap<>();
     }
 
     public void parseTheQuery(String query) {
@@ -51,96 +51,91 @@ public class Searcher {
         Integer[] dictionaryLoc;
         for (Map.Entry<String, Integer[]> entry : queryParsed.entrySet()) {
             String key = entry.getKey();
-            if (dictionary.containsKey(key.toLowerCase())) {
-                if (!wordAndLocations.containsKey(key)){
+            String keyLower = key.toLowerCase();
+            String keyUpper = key.toUpperCase();
+            if (dictionary.containsKey(keyLower)) {
+                if (!wordAndLocations.containsKey(key)) {
                     dictionaryLoc = entry.getValue();
-                    wordAndLocations.put(key, getAllPostings(dictionaryLoc[0]));
+                    Integer[] fromDictionary = dictionary.get(keyLower);
+                    wordAndLocations.put(key, getAllPostings(fromDictionary[0]));
                 }
-            } else if (dictionary.containsKey(key.toUpperCase())) {
-                if(!wordAndLocations.containsKey(key)){
+            } else if (dictionary.containsKey(keyUpper)) {
+                if (!wordAndLocations.containsKey(key)) {
                     dictionaryLoc = entry.getValue();
-                    wordAndLocations.put(key,getAllPostings(dictionaryLoc[0]));
+                    Integer[] fromDictionary = dictionary.get(keyUpper);
+                    wordAndLocations.put(key, getAllPostings(fromDictionary[0]));
                 }
             } else {
                 System.out.println("we don't have this word in our dictionary");
             }
         }
+
     }
 
 
     public ArrayList<String> getAllPostings(int lineNumber) {
         String line = "";
         String namePost = "";
-        String post="";
+        String post = "";
         if (stem)
-            namePost = "postFileWithoutStem";
+            namePost = "Stem\\postFileWithStem";
         else
-            namePost = "postFileWithStem";
+            namePost = "WithoutStem\\postFileWithoutStem";
         try (Stream<String> lines = Files.lines(Paths.get(postPath + "\\" + namePost + ".txt"))) {
             line = lines.skip(lineNumber).findFirst().get();
-            TermPost term=new TermPost(line);
-            post=term.getPost();
+            TermPost term = new TermPost(line);
+            post = term.getPost();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-        if (!post.equals("")){
-            ArrayList<String> arr= mySplit(post," ");
+        if (!post.equals("")) {
+            ArrayList<String> arr = mySplit(post, " ");
+            return arr;
+
         }
         return null;
     }
 
     public void createCountWordsQuery(String query) {
-        ArrayList<String> tmp=mySplit(query," ");
-        for(int i=0;i<tmp.size();i++){
-            String key=tmp.get(i);
-            if(!countWordsQuery.containsKey(key)){
-                countWordsQuery.put(key,1);
-            }else {
-                countWordsQuery.put(key,countWordsQuery.get(key)+1);
+        ArrayList<String> tmp = mySplit(query, " ");
+        for (int i = 0; i < tmp.size(); i++) {
+            String key = tmp.get(i);
+            if (!countWordsQuery.containsKey(key)) {
+                countWordsQuery.put(key, 1);
+            } else {
+                countWordsQuery.put(key, countWordsQuery.get(key) + 1);
             }
         }
     }
 
-    public void sendToRanker(){
-        Ranker ranker=new Ranker();
-        ranker.rankBM25(docsContainsQuery,countWordsQuery,numOfDocs);
+    public void sendToRanker() {
+        Ranker ranker = new Ranker();
+        ranker.rankBM25(docsContainsQuery, countWordsQuery, numOfDocs);
     }
-
-//    public HashMap<String, ArrayList<String>> createit(){
-//        HashMap<String, ArrayList<String>> arr=new HashMap<>();
-//        ArrayList<String> tmp=mySplit("LA100590-0148:1:207/1362 LA041689-0056:2:334/659 LA042190-0155:1:981/1037 LA050690-0199:1:964/1066 FT921-8666:1:271/471 bla"," ");
-//        arr.put("valances",tmp);
-//        tmp=mySplit("LA100590-0148:1:207/1362 LA041689-0056:2:334/659 LA042190-0155:1:981/1037 LA050690-0199:1:964/1066 FT921-8666:1:271/471 bla"," ");
-//        arr.put("valances1",tmp);
-//        tmp=mySplit("LA100590-0148:1:207/1362 LA041689-0056:2:334/659 LA042190-0155:1:981/1037 LA050690-0199:1:964/1066 FT921-8666:1:271/471 bla"," ");
-//        arr.put("valances2",tmp);
-//        tmp=mySplit("LA100590-0148:1:207/1362 LA041689-0056:2:334/659 LA042190-0155:1:981/1037 LA050690-0199:1:964/1066 FT921-8666:1:271/471 bla"," ");
-//        arr.put("valances3",tmp);
-//        return arr;
-//    }
 
 
     public void createDocsContainsQuery() {
-        for (Map.Entry<String, ArrayList<String>> entry : wordAndLocations.entrySet()){
-            ArrayList<String> tmp=entry.getValue();
-            String keyWord=entry.getKey();
-            for (int i=0;i<tmp.size()-1;i++){
-                String dfToAdd=tmp.get(tmp.size()-1);
-                String df=mySplit(dfToAdd,";").get(1);
-                ArrayList<String> docAndTf=mySplit(tmp.get(i),":");
-                String key=docAndTf.get(0);
-                ArrayList<String> tmp1=mySplit(docAndTf.get(2),"/");
-                if(docsContainsQuery.containsKey(key)){
-                    String[] strings={keyWord,docAndTf.get(1)+"",tmp1.get(1)+"",df};
+        for (Map.Entry<String, ArrayList<String>> entry : wordAndLocations.entrySet()) {
+            ArrayList<String> tmp = entry.getValue();
+            String keyWord = entry.getKey();
+            for (int i = 0; i < tmp.size() - 1; i++) {
+                String dfToAdd = tmp.get(tmp.size() - 1);
+                String df = mySplit(dfToAdd, ":").get(1);
+                ArrayList<String> docAndTf = mySplit(tmp.get(i), ":");
+                String key = docAndTf.get(0);
+                ArrayList<String> tmp1 = mySplit(docAndTf.get(2), "/");
+                if (docsContainsQuery.containsKey(key)) {
+                    String[] strings = {keyWord, docAndTf.get(1), tmp1.get(1), df, tmp1.get(0)};
                     docsContainsQuery.get(key).add(strings);
-                }else {
-                    docsContainsQuery.put(key,new ArrayList<>());
-                    String[] strings={keyWord,docAndTf.get(1)+"",tmp1.get(1)+"",df};
+                } else {
+                    docsContainsQuery.put(key, new ArrayList<>());
+                    String[] strings = {keyWord, docAndTf.get(1), tmp1.get(1), df, tmp1.get(0)};
                     docsContainsQuery.get(key).add(strings);
                 }
             }
         }
+
     }
 
     //create an Hash with all the stop words
@@ -159,8 +154,5 @@ public class Searcher {
         }
         return stopwords;
     }
-
-
-
 
 }
